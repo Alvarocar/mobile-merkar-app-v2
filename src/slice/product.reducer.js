f7e548bc4afe128bfb8c2be90c68ad3f0c88497d
Alvarocar <69275818+Alvarocar@users.noreply.github.com>
2022-11-08 21:11:47 -0500

    update product reducer

--- src/slice/product.reducer.js
+++ src/slice/product.reducer.js
@@ -1,5 +1,6 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import {ProductRepository} from '@src/repository/product.repository';
+import { isEmpty } from 'lodash';
 
 const productRepo = new ProductRepository();
 
@@ -7,6 +8,7 @@ const initialState = () => {
   return {
     products: [],
     productDetail: null,
+    total: 0,
   };
 };
 
@@ -21,6 +23,17 @@ export const GET_PRODUCT_BY_ID = createAsyncThunk(
   },
 );
 
+const updateTotal = callback => (state, action) => {
+  callback(state, action)
+  const p = state.products
+  if (isEmpty(p)) {
+    state.total = 0
+    return
+  }
+  state.total = p.reduce((a, b) => a.price + b.price)
+}
+
+
 const productSlice = createSlice({
   name: 'product',
   initialState: initialState(),
@@ -28,6 +41,27 @@ const productSlice = createSlice({
     CLEAN_DETAIL: state => {
       state.productDetail = null;
     },
+    SAVE_IN_PRODUCT_LIST: updateTotal(
+      (state, action) => {
+        state.products = [...state.products, action.payload]
+        state.total = state.products.reduce((a, b) => a.price + b.price)
+      }
+    )
+,
+    DELETE_PRODUCT_IN_LIST: updateTotal((state, action) => {
+      const id = action.payload
+      state.products = state.products.filter(p => p.id !== id)
+    }),
+    DELETE_GROUP_OF_PRODUCTS_IN_LIST: updateTotal((state, action) => {
+      /**
+       * @type {number[]}
+       */
+      const groupId = action.payload
+      state.products = state.products.filter(p => groupId.includes(p.id))
+    }),
+    DROP_PRODUCT_LIST: updateTotal(state => {
+      state.products = []
+    })
   },
   extraReducers: builder => {
     builder.addCase(GET_PRODUCT_BY_ID.fulfilled, (state, action) => {
@@ -42,7 +76,7 @@ const productSlice = createSlice({
 
 export default productSlice.reducer;
 
-export const {CLEAN_DETAIL} = productSlice.actions;
+export const {CLEAN_DETAIL, DELETE_GROUP_OF_PRODUCTS_IN_LIST, DELETE_PRODUCT_IN_LIST, DROP_PRODUCT_LIST, SAVE_IN_PRODUCT_LIST} = productSlice.actions;
 
 export const selectProducts = state => state.product.products;
 
